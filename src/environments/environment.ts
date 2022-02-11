6632ba6328a77b77642331d4f9c6daad93d35721
user <sivtsovatanya1985@gmail.com>
2022-02-11 13:21:14 +0200

    added some changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  versionName: '1.0.0',
+  firebase: {
+    apiKey: "AIzaSyALnW4unm24srzXZYYwqO7So2DQsfoDvkk",
+    authDomain: "angular-third.firebaseapp.com",
+    databaseURL: "https://angular-third-default-rtdb.firebaseio.com",
+    projectId: "angular-third",
+    storageBucket: "angular-third.appspot.com",
+    messagingSenderId: "57304976021",
+    appId: "1:57304976021:web:cfc714ff944c901c71299c",
+    measurementId: "G-FS3K19BE1Z"
+  },
 };
 
 /*
