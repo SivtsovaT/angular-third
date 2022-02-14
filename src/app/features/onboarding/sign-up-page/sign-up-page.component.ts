15b31d0c7df45c4ea96efc9097151e76027cb410
user <sivtsovatanya1985@gmail.com>
2022-02-14 14:17:50 +0200

    Added sign up with email page

--- src/app/features/onboarding/sign-up-page/sign-up-page.component.ts
+++ src/app/features/onboarding/sign-up-page/sign-up-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Auth, createUserWithEmailAndPassword} from "@angular/fire/auth";
+import {Router} from "@angular/router";
+import firebase from "firebase/compat";
+import FirebaseError = firebase.FirebaseError;
 
 @Component({
   selector: 'app-sign-up-page',
@@ -7,9 +11,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpPageComponent implements OnInit {
 
-  constructor() { }
+  credentials: SignUpCredentials = new SignUpCredentials();
+
+  constructor(private auth: Auth, private router: Router) {
+  }
 
   ngOnInit(): void {
   }
 
+  async handleSignUp() {
+    try {
+      await createUserWithEmailAndPassword(this.auth, this.credentials.email, this.credentials.password);
+      this.router.navigate(['home']);
+    } catch (e) {
+      let error = e as FirebaseError;
+      this.credentials.errorMessage = error.message;
+    }
+  }
+
+}
+
+class SignUpCredentials {
+  errorMessage?: string
+
+  private _email?: string;
+  private _password?: string;
+
+  get email() {
+    return this._email ?? '';
+  }
+
+  get password() {
+    return this._password ?? '';
+  }
+
+  set email(value: string) {
+    if (this.errorMessage) {
+      this.errorMessage = undefined;
+    }
+    this._email = value;
+  }
+
+  set password(value: string) {
+    if (this.errorMessage) {
+      this.errorMessage = undefined;
+    }
+    this._password = value;
+  }
 }
