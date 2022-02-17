fd6cc4c6d486bdaaa3de324119a171ddfdf20198
user <sivtsovatanya1985@gmail.com>
2022-02-17 15:50:18 +0200

    - Added facebook integration - Added error handling to bash script

--- src/app/features/onboarding/index-page/index-page.component.ts
+++ src/app/features/onboarding/index-page/index-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Auth, FacebookAuthProvider, signInWithPopup} from "@angular/fire/auth";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-index-page',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: Auth, private router: Router) {
+  }
 
   ngOnInit(): void {
   }
 
+  async continueFacebook() {
+    const provider = new FacebookAuthProvider();
+    provider.setCustomParameters({
+      'display': 'popup'
+    })
+    try {
+      const credentials = await signInWithPopup(this.auth, provider);
+      this.router.navigate(['home']);
+    } catch (e) {
+
+    }
+  }
 }
